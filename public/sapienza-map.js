8564e773d14140769b266b1f10c8fd6a6e017e7f
Federico Bednarski <bedna.bedna@gmail.com>
2019-04-17 18:11:10 +0200

    limit ranking to 5

--- public/sapienza-map.js
+++ public/sapienza-map.js
@@ -121,7 +121,7 @@ function successToast(message) { toast("success", message)}
 
 //  ----------------- TOAST END -----------------
 //  ----------------- RANKING START -----------------
-const RANKIN_LENGTH = 7; // numero di fazioni messe nella table ranking
+const RANKIN_LENGTH = 5; // numero di fazioni messe nella table ranking
 let factionsDepartments = {}
 function updateRanking() {
 	let factionsByRank = getFactionsByDepartments().slice(0, RANKIN_LENGTH)
