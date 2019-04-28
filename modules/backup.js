c32d3f81ac818b461da457994620fb9788c54273
jacopoRufini <jacopo@Jacopos-iMac.homenet.telecomitalia.it>
2019-04-28 14:11:07 +0200

    Revert "salvatore"
    This reverts commit c53e4cdff0f0cfa32fba82a50b8b4f07860aeac2.

--- modules/backup.js
+++ modules/backup.js
@@ -4,13 +4,23 @@ const Logger = require('./logger');
 const Factions = require("./factions")
 const dir = './backup/'
 
-const BACKUP = dir + 'backup.json'; /* inserire project path in caso di restore backup */
+const BACKUP = dir + 'data.json'; /* inserire project path in caso di restore backup */
+
+function deleteOldest(){
+  fs.readdir(folder, (err, files) => {
+    if (files.length >= 7){
+      fs.unlinkSync(dir + files[0]);
+    }
+  });
+}
 
 // save backup
 module.exports.saveBackup = function() {
+  deleteOldest();
   const object = Factions.getData();
   object['logs'] = Logger.getLogs();
-  fs.writeFile(BACKUP, JSON.stringify(object, null, 2), (err) => {});
+  let date = new Date();
+  fs.writeFile(dir + date, JSON.stringify(object, null, 2), (err) => {});
 }
 
 // load backup
